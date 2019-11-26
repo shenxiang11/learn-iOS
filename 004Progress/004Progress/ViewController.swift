2867bf464b02139705ed5eced44e7a49f3444742
香饽饽zizizi <863461783@qq.com>
2019-11-26 12:02:41 +0800

    004

---
+++ 004Progress/004Progress/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  004Progress
+//
+//  Created by 沈翔 on 2019/11/26.
+//  Copyright © 2019 沈翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetView: UIView!
+    
+    private let progress = MyLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        targetView.layer.addSublayer(progress)
+        progress.number = 0
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        progress.frame = targetView.bounds
+    }
+
+    @IBAction func onSliderValueChanged(_ sender: UISlider) {
+        progress.number = Double(sender.value)
+    }
+}
+
+class MyLayer: CALayer {
+    var number: Double = 0 {
+        didSet {
+            self.textLayer.string = String(format: "%.0lf%%", number * 100)
+            self.setNeedsDisplay()
+        }
+    }
+    
+    private let textLayer: CATextLayer = {
+        let textLayer = CATextLayer()
+        let font = UIFont(name: "PingFang SC", size: 20)!
+        textLayer.font = font.fontName as CFTypeRef
+        textLayer.foregroundColor = UIColor.black.cgColor
+        textLayer.fontSize = font.pointSize
+        textLayer.alignmentMode = .center
+        textLayer.contentsScale = UIScreen.main.scale
+        textLayer.isWrapped = true
+        textLayer.string = ""
+        return textLayer
+    }()
+
+    
+    override init() {
+        super.init()
+        self.addSublayer(textLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSublayers() {
+        super.layoutSublayers()
+        
+        let th = NSString("100%").boundingRect(with: CGSize(width: CGFloat.infinity, height: CGFloat.infinity), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: UIFont(name: "PingFang SC", size: 20)!], context: nil).height
+        
+        self.textLayer.frame = CGRect(x: 0, y: self.frame.height / 2 - th / 2, width: self.frame.width, height: th)
+    }
+    
+    override func draw(in ctx: CGContext) {
+        let radius = self.frame.width * 0.45
+        let center = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        
+        ctx.setStrokeColor(UIColor.gray.cgColor)
+        ctx.setLineWidth(radius * 0.03)
+        ctx.addEllipse(in: CGRect(x: center.x - radius, y: center.y - radius, width: radius * 2, height: radius * 2))
+        ctx.strokePath()
+        
+        ctx.setStrokeColor(UIColor.red.cgColor)
+        ctx.setLineWidth(radius * 0.08)
+        ctx.setLineCap(.round)
+        let endAngle = CGFloat(number) * CGFloat.pi * 2 - 0.5 * CGFloat.pi
+        ctx.addArc(center: center, radius: radius, startAngle: CGFloat.pi * -0.5, endAngle: endAngle, clockwise: false)
+        ctx.strokePath()
+    }
+}
+
---
+++ 004Progress/README.md
@@ -0,0 +1,7 @@
+## 简单原型进度条
+
+- UISlider 基本使用
+- CALayer 基本操作
+- 数字的字符串格式化方法
+- 变量的 didSet
+- NSString 测量文字占用的空间
